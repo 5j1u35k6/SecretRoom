e53114dab23ea5e9b26dedddf02ae2e292ce7da7
5j1u35k6 <max19450@gmail.com>
2026-07-11 23:01:38 +0800

    Remove retired X OAuth frontend config

--- sr_x_oauth_config.js
+++
@@ -1,9 +0,0 @@
-// Public X OAuth 2.0 configuration.
-// Client ID and Cloud Function URLs are public. Never place an X Client Secret here.
-window.SR_X_OAUTH_CONFIG = Object.freeze({
-  clientId: 'T3NtaWJ5cy1jTUh3Nk1ZNnlkY0c6MTpjaQ',
-  redirectUri: 'https://5j1u35k6.github.io/SecretRoom/',
-  startEndpoint: 'https://asia-east1-secretroom-ef728.cloudfunctions.net/xOAuthStart',
-  completeEndpoint: 'https://asia-east1-secretroom-ef728.cloudfunctions.net/xOAuthComplete',
-  scopes: Object.freeze(['tweet.read', 'users.read'])
-});
