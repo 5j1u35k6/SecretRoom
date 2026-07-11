e1ef62d61072452d299a4b8fe4810cad93559a48
5j1u35k6 <max19450@gmail.com>
2026-07-11 20:18:12 +0800

    Add public X OAuth configuration

---
+++ sr_x_oauth_config.js
@@ -0,0 +1,7 @@
+// Public X OAuth 2.0 configuration.
+// Client ID is public for a Single Page App. Never place an X Client Secret in this repository.
+window.SR_X_OAUTH_CONFIG = Object.freeze({
+  clientId: '',
+  redirectUri: 'https://5j1u35k6.github.io/SecretRoom/',
+  scopes: Object.freeze(['tweet.read', 'users.read'])
+});
